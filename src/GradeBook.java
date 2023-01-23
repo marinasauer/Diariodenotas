7f3429c5c435b5135da0a3a90795b76ea54dada8
marinasauer <marinarafaelasauer@gmail.com>
2023-01-23 19:25:49 -0300

    cria classe GradeBook com atributos com métodos get e set de nomeCurso

---
+++ src/GradeBook.java
@@ -0,0 +1,13 @@
+public class GradeBook {
+    private String nomeCurso;
+    double [] notasAlunos;
+
+    public String getNomeCurso(){
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+
+}
