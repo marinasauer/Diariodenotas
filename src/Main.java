b4e7ea89a3a3cc0d361e615be953f405ac929e38
marinasauer <marinarafaelasauer@gmail.com>
2023-01-24 19:41:09 -0300

    teste

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        GradeBook cursoJava = new GradeBook();
+        //GradeBook cursoJava = new GradeBook();
+
+        System.out.println("Quantidade ded alunos");
+        Scanner input = new Scanner(System.in);
+        double [] notasDoCurso = new double[input.nextInt()];
+        System.out.println("Digite as notas do curso");
+        for(int i=0;i< notasDoCurso.length;i++){
+            System.out.printf("Digite a nota do aluno %d: ",i+1);
+            notasDoCurso[i]= input.nextDouble();
+        }
+        GradeBook cursoJava = new GradeBook("Curso Java",notasDoCurso);
+        System.out.printf("Nome do curso:%s%n",cursoJava.getNomeCurso());
+        System.out.printf("Média do curso:%.2f%n",cursoJava.getMediaTurma());
+        System.out.printf("Menor nota do curso:%s%n",cursoJava.getMenorNota());
+
+
     }
 }
